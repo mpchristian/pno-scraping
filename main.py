b36ca3ad2172c4a38b196df94d83fbf471f18455
Christian Mendes Pereira <christianp3m@gmail.com>
2022-11-17 12:21:28 -0300

    feat: Use bib parsel to use selector for handling with the response

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import requests
+from parsel import Selector
 
 
 # Requisição do tipo GET
 url = 'https://perguntarnaoofende.com/?s=roteiro'
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0 ' +
+    'Gecko/20100101 Firefox/55.0',
 }
 response = requests.get(url, headers=headers)
-print(response.status_code)
-print(response.text)
+selector = Selector(text=response.text)
+print(selector)
