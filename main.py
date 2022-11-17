814649f1fdb4e8516c3e5a03f346b40f1f9d1e53
Christian Mendes Pereira <christianp3m@gmail.com>
2022-11-17 13:30:00 -0300

    refactor: Put code in the new function fetch

--- main.py
+++ main.py
@@ -2,14 +2,35 @@ import requests
 from parsel import Selector
 
 
+def fetch(url):
+    """Recebe uma URL;
+    Faz uma requisição HTTP GET para esta URL;
+    Caso requisição bem sucedida: retorna com status 200 o conteúdo de texto;
+    Caso resposta com status diferente de 200: retorna None;
+    Caso Timeout de 3 segundos: retorna None.
+    """
+
+    try:
+        headers = {
+          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; ' +
+          'rv:55.0 Gecko/20100101 Firefox/55.0',
+        }
+        response = requests.get(
+            url, headers=headers, timeout=3
+        )
+        if response.status_code == 200:
+            return response.text
+        return None
+    except requests.ReadTimeout:
+        return None
+
+
 # Requisição do tipo GET
 url = 'https://perguntarnaoofende.com/?s=roteiro'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0 ' +
-    'Gecko/20100101 Firefox/55.0',
-}
-response = requests.get(url, headers=headers)
-selector = Selector(text=response.text)
+text_content = fetch(url)
+
+
+selector = Selector(text=text_content)
 
 
 # Pegar artigos da primeira pagina
