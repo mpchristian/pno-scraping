fb7fb9c5c8b95d28223596ee18eac9069eb76341
Christian Mendes Pereira <christianp3m@gmail.com>
2022-11-17 14:08:07 -0300

    feat: Get the URLs newt page with scrape_next_page_link and get structured infos of an article with scrape_article

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from parsel import Selector
+# import urllib.request
 
 
 # Functions
@@ -28,7 +29,7 @@ def fetch(url):
 
 def scrape_articles(html_content, css_selector):
     """It recieves a string with the HTML content and CSS selector;
-    Scrapes the recieved contentand returns a list of the articles URLs;
+    Scrapes the recieved content and returns a list of the articles URLs;
     If no one article is found, returns ampty list
     """
     selector = Selector(text=html_content)
@@ -36,10 +37,52 @@ def scrape_articles(html_content, css_selector):
     return urls
 
 
+def scrape_next_page_link(html_content, css_selector):
+    """It recieves a string with the HTML content and CSS selector;
+    Scrapes the recieved content and returns the URL of the next page;
+    If the link is not found, returns None
+    """
+    selector = Selector(text=html_content)
+    url = selector.css(css_selector).get()
+    return url
+
+
+def scrape_article(html_content):
+    """Recebe uma string contendo o conteúdo HTML da página de uma notícia;
+    busca as informações das notícias e preenche um dicionário com os atributos
+        * url - link para acesso da notícia.
+    """
+    selector = Selector(text=html_content)
+    url = selector.css("head link[rel='canonical']::attr(href)").get()
+    title = selector.css(".entry-title::text").get().rstrip()
+    pdf_url = selector.css(".wp-block-button__link::attr(href)").get()
+
+    # ---------- comments_count
+    comments_count = (
+        selector.css(".pk-share-buttons-count.cs-font-primary::text").get()
+        or "0"
+    )
+    comments_count = [int(s) for s in comments_count.split() if s.isdigit()]
+    # -----------
+
+    data = {}
+    data["url"] = url
+    data["title"] = title
+    data["pdf_url"] = pdf_url
+    return data
+
+
 # Execute
 url = 'https://perguntarnaoofende.com/?s=roteiro'
 text_content = fetch(url)
-urls = scrape_articles(text_content, ".entry-title.h2 a::attr(href)")
 
+urls = scrape_articles(
+  text_content,
+  ".entry-title.h2 a::attr(href)")
 
-print(urls)
+# next_page_link = scrape_next_page_link(
+#   text_content,
+#   "a.next.page-numbers::attr(href)")
+
+article = scrape_article(fetch(urls[0]))
+print(article)
